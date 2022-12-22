3e858c5fef9f0f5e60139ad909c0aaf9057bad91
saidovv <116736002+saidovv@users.noreply.github.com>
2022-12-22 15:26:40 +0500

    add

--- README.md
+++ README.md
@@ -10,11 +10,82 @@ Method and a function are the same, with different terms.A method is a procedure
 (...)
 Усул маҷмӯи амалҳои оқилонаест, ки барои ҳалли мушкилоти мушаххас ё ноил шудан ба ҳадафи мушаххас бояд анҷом дода шаванд.
 
+
+## Method 1
 ![](./image/foot1.jpg)
+
+## Method 2
 ![](./image/foot2.jpg)
 
 or
 
+## method 1
 ![](./image/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA.PNG)
+
+## method 2
 ![](./image/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA2.PNG)
 
+
+
+## Create JavaScript Strings
+
+![](./image/1.jpg)
+
+
+
+## method charAt()
+
+![](./image/2.jpg)
+
+
+## method concat()
+
+![](./image/3.jpg)
+
+
+## method replace()
+
+![](./image/4.jpg)
+
+
+## method split ()
+
+![](./image/5.jpg)
+
+
+## method substr (start, length)
+
+![](./image/6.jpg)
+
+
+## method substring(start,end)
+
+![](./image/7.jpg)
+
+
+## method slice(start, end)
+
+![](./image/8.jpg)
+
+
+## method toLowerCase() toUpperCase()
+
+![](./image/9.jpg)
+
+
+## method trim()
+
+![](./image/10.jpg)
+
+
+## method includes()
+
+![](./image/11.jpg)
+
+
+## method search ()
+
+![](./image/12.jpg)
+
+
+# THANKS FOR ATATION!
---
+++ main.js
@@ -0,0 +1,141 @@
+// let name1 ='Ilhom'
+// let name2="Ilhom"
+// let result=`The names are ${name1} and ${name2}`
+
+
+
+
+
+// charAt ()
+
+// let name1='Ilhom'
+// let result=name1.charAt(0)
+// console.log(result)       
+
+// I
+
+
+
+
+// concat ()
+
+// let firstname='Ilhom'
+// let lastname='Saidov'
+// let result=firstname.concat(' ', lastname)
+// console.log(result)
+
+// Ilhom Saidov
+
+
+
+
+// replace ()
+
+// let text ='Ecma Script'
+// let result=text.replace('Ecma','Java')
+// console.log(result)
+
+// Java Script
+
+
+
+
+// split ()
+
+// let text='Samsung'
+// let result=text.split();
+// console.log(result)
+
+// ['Samsung']
+
+
+// let text2='Samsung is a brand'
+// let result2=text2.split(' ')
+// console.log(result2)
+
+// ['Samsung', 'is', 'a', 'brand']
+
+
+
+
+
+// substr (start, length)
+
+// let text='Ilhom Saidov'
+// let result=text.substr(0,4)
+// console.log(result)
+
+// Ilho
+
+
+
+
+// substring (start, end)
+
+// let text='Ilhom Saidov'
+// let result=text.substring(3)
+// console.log(result)
+
+// om Saidov
+
+
+
+
+// slice(start, end)
+
+// let text='Ilhom Saidov'
+// let result=text.slice(0,5)
+// console.log(result)
+
+// Ilhom
+
+
+
+
+// toLowerCase()     toUpperCase()
+
+// let text='Ilhom Saidov'
+// let result=text.toLowerCase()
+// console.log(result)
+
+// ilhom saidov
+
+// let text2='Ilhom Saidov'
+// let result2=text.toUpperCase()
+// console.log(result2)
+
+// ILHOM SAIDOV
+
+
+
+
+// trim ()
+
+// let text='           Ilhom Saidov             '
+// let result=text.trim()
+// console.log(result)
+
+// Ilhom Saidov
+
+
+
+
+
+// includes ()
+
+// let text='I am a frontend developer'
+// let result=text.includes('frontend')
+// console.log(result)
+
+// true
+
+
+
+
+// search ()
+
+// let text='Mr Red have a red apple'
+// let result=text.search('Red')
+// console.log(result)
+
+// 3
